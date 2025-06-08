9ac5b5f4f613cf497c1d5098fa898e234b6a325c
C1921 <w915504817@gmail.com>
2025-06-08 17:41:44 +0800

    侧边栏-自主创建响应式

---
+++ src/components/MySidebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <aside id="default-sidebar" class="w-12 md:w-64 h-screen transition-[width] duration-300">
+        <div class="px-0 md:px-2 pt-4 h-full transition-[padding] duration-300 delay-150">
+            <ul class="menu p-0">
+                <SidebarItem label="首页" icon="tabler--home" />
+                <SidebarItem label="账户" icon="tabler--user" active />
+                <SidebarItem label="通知" icon="tabler--message" />
+                <SidebarItem label="邮件" icon="tabler--mail" />
+                <SidebarItem label="日历" icon="tabler--calendar" />
+                <SidebarItem label="产品" icon="tabler--shopping-bag" />
+                <SidebarItem label="登录" icon="tabler--login" />
+                <SidebarItem label="登出" icon="tabler--logout-2" />
+            </ul>
+        </div>
+    </aside>
+</template>
+
+<script setup>
+import SidebarItem from './SidebarItem.vue';
+</script>
---
+++ src/components/SidebarItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+        <a :href="href" :class="{'menu-active': active}" class="h-11 flex items-center px-0 md:px-2 transition-all duration-300 relative">
+            <div class="w-12 flex justify-center md:w-auto md:justify-start">
+                <span :class="`icon-[${icon}] size-5 transition-all duration-300`"></span>
+            </div>
+            <span class="overflow-hidden transition-all duration-300 text-[0px] md:text-base opacity-0 md:opacity-100 ml-0 md:ml-2">
+                {{ label }}
+            </span>
+        </a>
+    </li>
+</template>
+
+<script setup>
+defineProps({
+    label: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        default: 'tabler--user'
+    },
+    active: {
+        type: Boolean,
+        default: false
+    },
+    href: {
+        type: String,
+        default: '#'
+    }
+});
+</script> 
