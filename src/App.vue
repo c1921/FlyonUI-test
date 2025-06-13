15978b3501ed7d4a7fdcecc80b7b93616c961ac0
C1921 <w915504817@gmail.com>
2025-06-13 17:53:24 +0800

    新增 Charts 组件

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import Radio from './components/Radio.vue';
 import ToggleCount from './components/ToggleCount.vue';
 import RangeDemo from './components/RangeDemo.vue';
 import Tables from './components/Table.vue';
+import Charts from './components/Charts.vue';
 
 // 当前激活的菜单项
 const activeMenu = ref('home'); // 默认激活首页
@@ -22,7 +23,7 @@ const handleMenuChange = (menu: string) => {
 <template>
   <div>
     <Sidebar @menu-change="handleMenuChange" />
-    <div class="p-4 space-y-6 ml-14 md:ml-64 transition-all duration-500">
+    <div class="ml-14 md:ml-64 transition-all duration-500">
       <!-- 首页内容 -->
       <template v-if="activeMenu === 'home'">
         <Radio />
@@ -33,7 +34,10 @@ const handleMenuChange = (menu: string) => {
       <template v-if="activeMenu === 'transaction'">
         <Tables />
       </template>
+
+      <template v-if="activeMenu === 'production'">
+        <Charts />
+      </template>
     </div>
-    <div></div>
   </div>
 </template>
